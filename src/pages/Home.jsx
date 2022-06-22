81298c3fe992f6bca6cf7f84900e564a12b61a07
krisnapramulyap <19560029@stmik.jayakarta.ac.id>
2022-06-22 22:15:11 +0700

    add carousel and float button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< Updated upstream
 import { HomeNav2 } from "./components/Navbar/Navbar"
 import "../css/style.css";
 
@@ -14,3 +15,21 @@ function Home() {
     );
 }
 export default Home;
+=======
+import { HomeNavbar } from "./components/Navbar/Navbar"
+import { Carousel } from "./components/Carousel/Carousel"
+import { FloatButton } from "./components/FloatButton/Floatbutton"
+import "../css/style.css";
+export default function Home() {
+
+    return (
+
+            <div>
+                <HomeNavbar />
+                <Carousel />
+                <FloatButton />
+            </div>
+
+    );
+}
+>>>>>>> Stashed changes
