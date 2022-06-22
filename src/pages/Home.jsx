f3c2ec23ae0a1240de79a0e9c2a6dbfb52c7cbd1
krisnapramulyap <19560029@stmik.jayakarta.ac.id>
2022-06-22 22:16:39 +0700

    fix merge conflit home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,3 @@
-import React from "react";
-<<<<<<< Updated upstream
-import { HomeNav2 } from "./components/Navbar/Navbar"
-import "../css/style.css";
-
-function Home() {
- 
-    return  (
-
-        <div>
-            <HomeNav2 />
-        </div>
-
-    
-    );
-}
-export default Home;
-=======
 import { HomeNavbar } from "./components/Navbar/Navbar"
 import { Carousel } from "./components/Carousel/Carousel"
 import { FloatButton } from "./components/FloatButton/Floatbutton"
@@ -31,5 +13,4 @@ export default function Home() {
             </div>
 
     );
-}
->>>>>>> Stashed changes
+}hanges
