d416da7549fbd2f57e0b0419270877488d78aaeb
indy <maharanindya2@gmail.com>
2022-07-06 17:13:10 +0700

    tampilan

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import ProductPage from "./pages/ProductPage";
 import InfoProduct from "./pages/InfoProduct";
 import CreateProduct from "./pages/CreateProduct";
 import Profile from "./pages/Profile";
+import InfoBidder from "./pages/InfoBidder";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -25,6 +26,8 @@ render(
         <Route path="/infoproduk" element={<InfoProduct />} />
         <Route path="/buatproduk" element={<CreateProduct />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/infopenawar" element={<InfoBidder />} />
+
       </Routes>
     </Router>
   </Provider>,
