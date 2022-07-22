7dea04a9d747a42469211b61c1d8662647e023f4
krisnapramulyap <19560029@stmik.jayakarta.ac.id>
2022-07-22 22:22:37 +0700

    95% done

--- src/pages/components/Navbar/Navbar.jsx
+++ src/pages/components/Navbar/Navbar.jsx
@@ -4,12 +4,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../../../slices/userSlice";
 import { addSearch } from "../../../slices/searchingSlice";
-import { Navbar, Container, Button, Dropdown, Offcanvas } from "react-bootstrap";
+import { Navbar, Container, Button, Dropdown, Offcanvas, Stack, Popover, Row, OverlayTrigger } from "react-bootstrap";
 import { FiLogIn, FiList, FiBell, FiUser, FiLogOut } from "react-icons/fi";
 import { styled } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import axios from "axios";
+import dateFormat from "dateformat";
 import "../Navbar/navbar.css";
 
 const Search = styled('div')(({ theme }) => ({
@@ -48,9 +49,15 @@ export function HomeNavbar() {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
     const [user, setUser] = useState({});
     const [data, setData] = useState({});
+    const [notif, setNotif] = useState([]);
+    const [notifStatus, setNotifStatus] = useState([]);
     const [open, setOpen] = React.useState(true);
     const [show, setShow] = useState(false);
     const [searching, setSearching] = useState("");
+    console.log(user.id)
+    console.log(data.id)
+    console.log(notif)
+
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -64,6 +71,7 @@ export function HomeNavbar() {
             addSearch(searching)
         )
     }
+
     const getUsers = async () => {
         try {
             const token = localStorage.getItem("token");
@@ -79,7 +87,11 @@ export function HomeNavbar() {
         }
     }
 
+
     useEffect(() => {
+
+
+
         const fetchData = async () => {
             try {
                 // Check status user login
@@ -95,23 +107,24 @@ export function HomeNavbar() {
                         },
                     }
                 );
-                // console.log(currentUserRequest.data.data.id)
+                console.log(currentUserRequest)
 
-                const currentUserResponse = currentUserRequest.data.data.id;
+                const currentUserResponse = currentUserRequest.data;
 
-                // console.log(currentUserResponse)
+                console.log(currentUserResponse.status)
 
                 if (currentUserResponse.status) {
                     dispatch(
                         addUser({
-                            user: currentUserResponse.user,
+                            user: currentUserResponse.data,
                             token: token,
                         })
                     );
-                    setUser(currentUserResponse.user);
+                    localStorage.setItem("user", JSON.stringify(currentUserResponse.data))
+                    setUser(currentUserResponse.data);
                 }
-                // console.log(addUser)
-                // console.log(setUser)
+                console.log(addUser)
+                console.log(setUser)
             } catch (err) {
                 setIsLoggedIn(false);
             }
@@ -121,6 +134,39 @@ export function HomeNavbar() {
         getUsers();
     }, [searching]);
 
+    useEffect(() => {
+        const notifikasi = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const user_local = localStorage.getItem("user");
+                const user = JSON.parse(user_local);
+
+                const notifRequest = await axios.get(`http://localhost:2000/v1/transaction/notif/${user.id}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    })
+                console.log(notifRequest);
+
+                const notifResponse = await notifRequest.data.data.posts;
+                console.log(notifResponse);
+                setNotif(notifResponse);
+
+                const notifResponseStatus = await notifRequest.data.message;
+                console.log(notifResponseStatus);
+                setNotifStatus(notifResponseStatus);
+
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        notifikasi();
+    }, [])
+
+
     const logout = () => {
         localStorage.removeItem("token");
 
@@ -128,6 +174,49 @@ export function HomeNavbar() {
         setUser({});
         navigate("/");
     };
+
+
+    const popoverNotif = (
+        <Popover id="popover-basic" className="box-shadow radius-primary" style={{ maxWidth: "376px" }}>
+            <Popover.Header className="radius-primary bg-white border-0 overflow-auto  " style={{ height: "400px" }}>
+                {notif.map((notif) =>
+                    data.id === notif.owner_id || notif.product.sold ? (
+                        <Row className="mb-0">
+                            <Link className="text-decoration-none text-black" to={`/infopenawar/${notif.id}`}>
+                                <Stack direction="horizontal" gap={3}>
+                                    <img src={`${notif.product.picture}`} alt=""
+                                        style={{ width: "48px", height: "48px", objectFit: "cover", borderRadius: "12px" }} />
+                                    <Stack>
+                                        <p className="m-0 text-black-50 fs-8">
+                                            {user.id === notif.owner_id && notif.product.sold ?
+                                                "Berhasil Terjual" : user.id === notif.user_id && notif.product.sold ?
+                                                    "Berhasil Ditawar" : notif.accepted === "reject" ?
+                                                        "Penawaran ditolak" : "Penawaran Produk"}
+                                        </p>
+                                        <p className="m-0 text-black">{notif.product.name}</p>
+                                        <p className={data.id === notif.data_id && notif.product.sold ? "m-0 text-black text-decoration-line-through" : "m-0 text-black"}>
+                                            {(notif.product.price)}
+                                        </p>
+                                        <p className="m-0 text-black">{data.id === notif.owner_id ? "ditawar " : "Berhasil Menawar "}
+                                            {(notif.requestedPrice)}
+                                        </p>
+                                        <p className="m-0 text-black-50 fs-8">
+                                            {data.id === notif.user_id && notif.product.sold === true ?
+                                                "Kamu akan segera dihubungi penjual via whatsapp" : ""}
+                                        </p>
+                                    </Stack>
+                                    <Stack>
+                                        <p className="m-0 ms-auto text-black-50 fs-8">{dateFormat(notif.createdAt, "d mmm, h:MM")}</p>
+                                    </Stack>
+                                </Stack>
+                            </Link>
+                            <hr />
+                        </Row>
+                    ) : ("")).reverse()
+                }
+            </Popover.Header>
+        </Popover>
+    );
 
 
     return (
@@ -174,7 +263,9 @@ export function HomeNavbar() {
                                             <FiList className="icon-list-header m-3" />
                                         </Button>
                                     </Link>
-                                    <FiBell className="icon-bell-header m-3" />
+                                    <OverlayTrigger trigger="click" placement="bottom" overlay={popoverNotif} >
+                                        <Button variant="light"> <FiBell className=" mb-1" />  </Button>
+                                    </OverlayTrigger>
 
                                     <Button
                                         variant="none"
@@ -196,9 +287,9 @@ export function HomeNavbar() {
                                             <Offcanvas.Title className="title-navbar-mobile">Second Hand</Offcanvas.Title>
                                         </Offcanvas.Header>
                                         <Offcanvas.Body>
-                                            <Dropdown.Item href="#/action-1">Notifikasi</Dropdown.Item>
-                                            <Dropdown.Item className="mt-2" href="#/action-1">Daftar Jual</Dropdown.Item>
-                                            <Dropdown.Item className="mt-2" href="#/action-1">Akun Saya</Dropdown.Item>
+                                            <Dropdown.Item href="#">Notifikasi</Dropdown.Item>
+                                            <Dropdown.Item className="mt-2" href={`/dashboardseller/${data.id}`}>Daftar Jual</Dropdown.Item>
+                                            <Dropdown.Item className="mt-2" href="/profile">Akun Saya</Dropdown.Item>
                                             <Button
                                                 variant="danger"
                                                 className="mx-2 mt-5"
